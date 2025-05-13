fc72a1777e98be4c8f8688ef59fbb28523ff471a
TheFederalNews <awsstg@thefederal.in>
2025-05-13 10:37:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  server: {
+    hmr: mode !== 'production' // HMR is only active in development
+  },
+  build: {
+    outDir: 'dist', // optional: ensure build output goes to the correct folder
+  },
+}))
