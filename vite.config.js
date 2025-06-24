0b788b1e6993299674fa2166fe4a1df15b38c277
TheFederalNews <awsstg@thefederal.in>
2025-06-24 15:32:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // or use '0.0.0.0' to expose to local network
+    port: 5173,
+    hmr: {
+      protocol: 'ws',
+      host: 'products.thefederal.com', // your custom domain
+      port: 5173,
+    },
+  },
 })
