e9f7f81853ba49ba09fe69960a549e40843da1b0
TheFederalNews <awsstg@thefederal.in>
2025-06-24 15:42:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
     port: 5173,
-    https: {
-      key: fs.readFileSync('./products.thefederal.com-key.pem'),
-      cert: fs.readFileSync('./products.thefederal.com.pem'),
-    },
+    https: true, // Enable HTTPS on dev server
     hmr: {
-      protocol: 'wss',
+      protocol: 'wss', // Secure WebSocket
       host: 'products.thefederal.com',
       port: 5173,
     },
