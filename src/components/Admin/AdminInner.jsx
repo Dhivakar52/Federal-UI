cb17f5c7b1e976b5a5a0c66252b4eb489a6966ac
TheFederalNews <awsstg@thefederal.in>
2025-06-25 12:24:33 +0530

    Update AdminInner.jsx

--- src/components/Admin/AdminInner.jsx
+++ src/components/Admin/AdminInner.jsx
@@ -197,7 +197,7 @@ const filteredUsers = users
                 Add User <span><Plus size={15} /></span>
               </button>
             </div>
-              <div className=" col-2 mb-3">
+              <div className=" col-7 mb-3 text-end">
               {/* <button
                 className="btn btn-primary"   
               >
